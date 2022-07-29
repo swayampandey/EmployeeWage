bf3d8e5b14fe73f9a78f811332bf7b0b2045bdec
swayampandey <swayam265@gmail.com>
2022-07-29 20:47:03 +0530

    [Swayam]: Refactor UC5 Monthly Emp Wage

--- src/com/bridegelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridegelabz/EmployeeWage/EmployeeWage.java
@@ -1,61 +1,41 @@
 package com.bridegelabz.EmployeeWage;
 
 public class EmployeeWage {
-	public static final int EmployeePresent=1;
-	public static final int EmployeePartTime=2;
-	public static final int WagePerHour=20;
+	public static final int PartTime=1;
+	public static final int FullTime=2;
+	public static final int RatePerHour=20;
 	public static final int WorkingDays=20;
-	public static final int MonthMaxHours=100;
 	
-
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		System.out.println("********************************************");
 		/*
-		 * Added UC6
+		 * Refactoring  UC5
 		 */
-		int numberOfHours=0;
-		int EmpWagePerDay=0, nHours=0;
-		int totalWorkingDays=0, totalWorkingHours=0;
-		int totalEmployeeWage=0;
-		while(totalWorkingHours<MonthMaxHours && totalWorkingDays<WorkingDays) {
-		int isPresent =(int) Math.floor(Math.random()*10) % 3;
-		switch (isPresent) {
-		
-		case EmployeePresent: 
-			numberOfHours= 8;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is Present");
-			System.out.println("Employee Wage for a day is "+numberOfHours*WagePerHour);
-			totalWorkingDays++;
-			nHours+=numberOfHours;
-			break;
+		// variables
+		int EmpHours=0, EmpWage=0, TotalEmpWage=0;
+		for(int day=0; day<WorkingDays; day++) {
+			int isPresent= (int)Math.floor(Math.random()*10)%3;
+			switch(isPresent) {
 			
-		case EmployeePartTime:
-			numberOfHours= 4;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is doing part time");
-			System.out.println("Employee part time wage for a day is "+numberOfHours*WagePerHour);
-			totalWorkingDays++;
-			nHours+=numberOfHours;
-			break;
-		
-		default :
-			numberOfHours= 0;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is Absent");
-			System.out.println("Employee Wage for day is 0");
-			break;
-			}
-		System.out.println("\ndays  = " + totalWorkingDays + " No. of Hours = " +nHours);
-		System.out.println(	"\nEmployee Monthly Earn for Max"
-				+ " Days or Hours = " + ((numberOfHours) * WagePerHour) * WorkingDays);
-		
-		totalEmployeeWage= totalWorkingHours * WagePerHour;
-		System.out.println("\nTotal Employee Wage without Condition is "+ totalEmployeeWage);
-		
-
+			case PartTime:
+				EmpHours=4;
+				break;
+			case FullTime:
+				EmpHours=8;
+				break;
+			default:
+				EmpHours=0;
+				}
+			EmpWage= EmpHours*RatePerHour;
+			TotalEmpWage +=EmpWage;
+			System.out.println("Employee Wage = " +EmpWage);
 		}
+			System.out.println("Total Employee Wage  = " +TotalEmpWage);
+		
+		}
+		
 
 	}
-}
+
