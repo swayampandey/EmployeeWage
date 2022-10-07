3727245c06cf4ce914f9a5f10523c37287cea18f
Swayam Prakash <swayam265@gmail.com>
2022-10-07 14:46:02 +0530

    [Swayam] : added functionality of monthly emp wage with maximum hrs and days condition computation in employee wage computation program

--- src/com/bridgelabz/EmployeeWageMain.java
+++ src/com/bridgelabz/EmployeeWageMain.java
@@ -7,37 +7,41 @@ public class EmployeeWageMain {
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program\n");
 
 		int empHour = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		/**
-		 * Generating random number using random class random.nextInt(2) will generate
-		 * random number between 0 and 3
-		 */
-		for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHours = 0;
+		int totalWorkingDays = 0;
+		
+		while(totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		
+			totalWorkingDays++;
 		Random random = new Random();
 		int attendance = random.nextInt(3);
 		switch (attendance) {
 		case IS_PART_TIME:
+			System.out.println("Part Time");
 			empHour = 4;
-			System.out.println("Day "+day+ " Part Time");
+			
 			break;
 		case IS_FULL_TIME:
+			System.out.println("Full Time ");
 			empHour = 8;
-			System.out.println("Day "+day+ " Full Time ");
+			
 			break;
 		default:
+			System.out.println("ABSENT");
 			empHour = 0;
-			System.out.println("Day "+day+" ABSENT");
+			
 		}
-		empWage = empHour * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		System.out.println("Day "+day+" Emp Wage : " +empWage);
+		totalEmpHours += empHour;
+		System.out.println("Day "+totalWorkingDays+ " Emp Hour: "+empHour);
+		
 		}
+		int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage : " + totalEmpWage);
 
 	}
